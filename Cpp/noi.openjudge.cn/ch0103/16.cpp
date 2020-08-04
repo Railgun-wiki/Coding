005330eff88d73707a07b9c86a0559abf86db14a
Railgun-wiki <laoshancun2006@126.com>
2020-08-04 08:26:00 +0800

    Add Openjudge

--- Cpp/noi.openjudge.cn/ch0103/16.cpp
+++ Cpp/noi.openjudge.cn/ch0103/16.cpp
@@ -3,6 +3,7 @@
 #include<cstdio>
 using namespace std;
 int main(){
+    std::ios::sync_with_stdio(false);
     int x1,y1,x2,y2;
     cin>>x1>>y1>>x2>>y2;
     double ans=sqrt(pow(abs(x2-x1),2)+pow(abs(y2-y1),2));
