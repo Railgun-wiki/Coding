090daa01af4fee0c2ac59cca392c32c243bd7d97
Railgun-wiki <laoshancun2006@126.com>
2020-09-20 11:44:07 +0800

    Add P1002

--- Cpp/Luogu/P1002.cpp
+++ Cpp/Luogu/P1002.cpp
@@ -4,34 +4,36 @@ int hx,hy,bx,by,ans;
 bool Map[21][21];
 int Move[8][2]=
 {
-    {-1,2},
     {-2,1},
-    {2,1},
-    {1,2},
-    {-1,-2},
     {-2,-1},
-    {2,-1},
-    {1,-2}
+    {-1,2},
+    {-1,-2},
+    {1,2},
+    {1,-2},
+    {2,1},
+    {2,-1}
 };
 void DFS(int x,int y){
-    if(x<0||y<0||x>20||y>20)
+    if(x<0||y<0||x>hx||y>hy)
         return ;
-    if(!Map[y][x]){
-        if(x==bx&&y==by){
+    if(!Map[x][y]){
+        cout<<x<<" "<<y<<" "<<ans<<endl;
+        if(x==hx&&y==hy){
             ans++;
             return ;
         }
         DFS(y+1,x);
         DFS(y,x+1);
     }
-    else return ;
-    
+    else return ; 
 }
 int main(){
     cin>>hx>>hy>>bx>>by;
     for(int i=0;i<8;i++){
-        if(hx+Move[i][1]>=0&&hy+Move[i][0]>=0&&hx+Move[i][1]<21&&hy+Move[i][0]<21)
-            Map[hy+Move[i][0]][hx+Move[i][1]]=1;
+	    if(bx+Move[i][0]>=0&&by+Move[i][1]>=0&&bx+Move[i][0]<=hx&&by+Move[i][1]<=hy){
+		    Map[Move[i][0]+bx][Move[i][1]+by]=1;
+            cout<<Move[i][0]+bx<<" "<<Move[i][1]+by<<endl;
+	    }
     }
     DFS(0,0);
     cout<<ans<<endl;
